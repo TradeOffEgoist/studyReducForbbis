3e9a24db4579bd578b8cc5a798a63104724704af
TradeOffEgoist <egoist116@naver.com>
2024-10-03 14:59:29 +0900

    action

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
